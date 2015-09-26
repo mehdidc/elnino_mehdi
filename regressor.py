9f8693dd823ca6ef704d67074bb1bff8cf5489b3
mehdi <mehdi@cherti.name>
2015-09-26 13:10:40 -0600

    earth

--- regressor.py
+++ regressor.py
@@ -1,9 +1,17 @@
-from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.base import BaseEstimator
+from pyearth import Earth
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = GradientBoostingRegressor(n_estimators=100, max_features="sqrt", max_depth=6)
+        self.clf = make_pipeline(
+                StandardScaler(),
+                PCA(n_components=200),
+                Earth(max_terms=20, max_degree=10)
+        )
 
     def fit(self, X, y):
         self.clf.fit(X, y.ravel())
